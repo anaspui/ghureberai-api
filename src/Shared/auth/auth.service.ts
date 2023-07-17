19310cf99dfa0cf00ebb7e2c2e117097b570994b
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-07-17 16:38:13 +0600

    Auth #4

--- src/Shared/auth/auth.service.ts
+++ src/Shared/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { User } from "../entities/user.entity";
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
-
+import * as bcrypt from "bcryptjs";
 @Injectable()
 export class AuthService {
 	constructor(
@@ -10,10 +10,18 @@ export class AuthService {
 		private userRepo: Repository<User>,
 	) {}
 
-	auth(Username: string) {
-		const user = this.userRepo.findOne({ where: { Username } });
+	async auth(Username: string) {
+		const user = await this.userRepo.findOne({ where: { Username } });
 		if (user) {
 			return user;
+		}
+	}
+
+	async decryptPassword(password: string, hashed: string): Promise<boolean> {
+		try {
+			return await bcrypt.compare(password, hashed);
+		} catch (error) {
+			throw new Error("Error comparing passwords: " + error.message);
 		}
 	}
 }
