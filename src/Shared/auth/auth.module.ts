272c050d3674bf07182762e81696798e278c45c5
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-07-19 01:28:32 +0600

    Json Web Token

--- src/Shared/auth/auth.module.ts
+++ src/Shared/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "../entities/user.entity";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
+import { JwtService } from "@nestjs/jwt";
 
 @Module({
 	imports: [TypeOrmModule.forFeature([User])],
-	providers: [AuthService],
+	providers: [AuthService, JwtService],
 	controllers: [AuthController],
 })
 export class AuthModule {}
