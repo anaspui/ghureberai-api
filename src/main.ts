7a6ef59f3cac90bedc8a4e220ae8b820c664c775
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-08-30 18:17:44 +0600

    /User Cookie issue

--- src/main.ts
+++ src/main.ts
@@ -5,39 +5,21 @@ import * as session from "express-session";
 import { sessionConfig } from "./Shared/auth/session.config";
 import * as cookieParser from "cookie-parser";
 import * as process from "process";
-import { Request, Response } from "express";
-const cors = require("cors");
+
 const express = require("express");
 const app = express();
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-
-	app.use(cookieParser());
-	app.use(express.json());
-
-	app.use((req: Request, res: Response, next: Function) => {
-		const allowedOrigins = [
-			"http://localhost:3000",
+	app.enableCors({
+		origin: [
 			"https://ghureberai-a8umtdtbz-anaspui.vercel.app",
-		];
-		const origin = req.headers.origin as string;
-
-		if (allowedOrigins.includes(origin)) {
-			res.header("Access-Control-Allow-Origin", origin);
-			res.header("Access-Control-Allow-Credentials", "true");
-		}
-
-		res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-		res.header(
-			"Access-Control-Allow-Headers",
-			"Origin, X-Requested-With, Content-Type, Accept",
-		);
-		next();
+			"http://localhost:3000",
+		],
+		credentials: true,
 	});
-
+	app.use(cookieParser());
 	app.use(session(sessionConfig));
-
 	await app.listen(process.env.PORT || 8000);
 }
 
