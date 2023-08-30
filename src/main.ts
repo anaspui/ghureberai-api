367b92481028c5c438c33c45ed41d9b84e50e10f
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-08-30 17:23:55 +0600

    CORS CONFIG FOR VERCEL #5

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,17 @@ import * as session from "express-session";
 import { sessionConfig } from "./Shared/auth/session.config";
 import * as cookieParser from "cookie-parser";
 import * as process from "process";
-
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.enableCors({
-		origin: [
-			"https://ghureberai-a8umtdtbz-anaspui.vercel.app",
-			"http://localhost:3000",
-		],
-		credentials: true,
-	});
+	app.use(
+		cors({
+			credentials: true,
+		}),
+	);
 	app.use(cookieParser());
 	app.use(session(sessionConfig));
 	await app.listen(process.env.PORT || 8000);
